54849744d623a6b9f2a173d2fcf37d47a4b2dcd3
Sangram <Sangram1677@gmail.com>
2025-09-20 22:33:50 +0530

    datatype added

---
+++ Datatypes/datatype.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int a = 123;
+    cout << a <<endl;
+
+    char b = 'v';
+    cout << b <<endl;
+
+    bool b1 = true;
+    cout << b1 <<endl;
+
+    float f = 1.2;
+    cout << f <<endl;
+
+    double d = 1.23;
+    cout << d <<endl;
+
+    int size = sizeof(d);
+    cout << "Size of a is " << size <<endl;
+
+    // Typecasting
+
+    int s = 'a';
+    cout << s << endl;
+
+    char ch = 98;
+    cout << ch << endl;
+
+    char ch1 = 123456;
+    cout << ch1 << endl;
+
+}
