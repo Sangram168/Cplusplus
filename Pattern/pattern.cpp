6ebb10b115d426d4938e6056d52ef57332c2219c
Sangram <Sangram1677@gmail.com>
2025-09-25 23:01:56 +0530

    pattern questions added

--- Pattern/pattern.cpp
+++ Pattern/pattern.cpp
@@ -149,21 +149,232 @@ int main(){
     // }
 
     // Q-9
+    // int n;
+    // cin >> n;
+
+    // int row = 1;
+    // while (row<=n)
+    // {
+    //     int col = 1;
+    //     char ch = 'A'+row-1;
+    //     while (col<=n)
+    //     {
+    //         cout << ch;
+    //         col++;
+    //     }
+    //     cout<< endl;
+    //     row++;
+        
+    // }
+
+    // Q-10
+
+    // int n;
+    // cin >> n;
+
+    // int row = 1;
+    // while (row<=n)
+    // {
+    //     int col = 1;
+    //     char ch = 'A'+col-1;
+    //     while (col<=n)
+    //     {
+    //         cout << ch;
+    //         col++;
+    //         ch++;
+    //     }
+
+    //     cout<< endl;
+    //     row++;
+    // }
+
+    // Q-11
+
+    // int n;
+    // cin >> n;
+
+    // int row = 1;
+    // char counter = 'A';
+    // while (row<=n)
+    // {
+    //     int col = 1;
+    //     while (col<=n)
+    //     {
+    //         cout << counter;
+    //         col++;
+    //         counter++;
+    //     }
+
+    //     cout<<endl;
+    //     row++;
+        
+    // }
+    
+    // Q-12
+
+    // int n;
+    // cin >> n;
+
+    // int row = 1;
+    // while (row<=n)
+    // {
+    //     int col = 1;
+    //     while (col<=row)
+    //     {
+    //         char ch = 'A' + row - 1;
+    //         cout <<ch;
+    //         col++;
+    //     }
+
+    //     cout<< endl;
+    //     row++;
+        
+    // }
+
+    // Q-13
+
+    // int n;
+    // cin >> n;
+
+    // int row = 1;
+    // char value = 'A';
+    // while (row<=n)
+    // {
+    //     int col = 1;
+    //     while (col<=row)
+    //     {
+    //         cout << value;
+    //         col++;
+    //         value++;
+    //     }
+    //     cout<<endl;
+    //     row++;
+    // }
+
+    // Q-14
+
+    // int n;
+    // cin >> n;
+
+    // int row = 1;
+    // while (row<=n)
+    // {
+    //     int col = 1;
+
+    //     while (col<=row)
+    //     {
+    //         char ch = 'A'+row+col-2;
+    //         cout << ch;
+    //         col = col + 1;
+    //     }
+    //     cout<<endl;
+    //     row++;
+        
+    // }
+    
+    // Q-15
+
+    // int n;
+    // cin >> n;
+
+    // int row = 1;
+
+    // while (row<=n)
+    // {
+    //     int col = 1;
+    //     char start = 'A'+n-row;
+    //     while (col<=row)
+    //     {
+    //         cout<<start;
+    //         start++;
+    //         col++;
+    //     }
+    //     cout<<endl;
+    //     row++;
+        
+    // }
+
+    // Q-16
+
+    // int n;
+    // cin >> n;
+
+    // int row = 1;
+    // while (row<=n)
+    // {
+    //     int space = n-row;
+    //     while (space)
+    //     {
+    //         cout << " ";
+    //         space--;
+    //     }
+
+    //     int col = 1;
+    //     while (col<=row)
+    //     {
+    //         cout<< "*";
+    //         col++;
+    //     }
+
+    //     cout<< endl;
+    //     row++;
+        
+        
+    // }
+
+    // Q-17
+
+    // int n;
+    // cin >> n;
+
+    // int row = 1;
+    // while (row<=n)
+    // {
+    //     int col = 1;
+    //     while (col<=n-row+1)
+    //     {
+    //         cout << "*";
+    //         col++;
+    //     }
+    //     cout<<endl;
+    //     row++;
+    // }
+
+
+    // Q-18
+
     int n;
     cin >> n;
 
     int row = 1;
     while (row<=n)
     {
-        int col = 1;
-        char ch = 'A'+row-1;
-        while (col<=n)
+        int space = n-row;
+        while (space)
         {
-            cout << ch;
+            cout<< " ";
+            space--;
+        }
+
+        int col = 1;
+        while (col<=row)
+        {
+            cout<< col;
             col++;
         }
-        cout<< endl;
+
+        int start = row-1;
+        while (start)
+        {
+            cout<<start;
+            start--;
+        }
+        
+        cout<<endl;
         row++;
         
+        
     }
+    
+    
 }
